f2d6b7f1cdd6483a48a236ab7e1e7106fe4519e8
sahilsea <sa9504793957@gamil.com>
2023-12-24 12:45:04 +0530

    Add: Operators.py File

---
+++ Day 2/Operators.py
@@ -0,0 +1,18 @@
+3+2
+4-2
+3*2
+2/3
+# Exponents
+print(2**3)
+
+# Order of operations
+PEMDAS
+()
+**
+* /
++ -
+
+# Multiplication and Division are given same priority , so calculation is peformed that leftmost
+# Same applies for Addition and Substraction
+
+print(3 * 3 + 3 / 3 - 3)
