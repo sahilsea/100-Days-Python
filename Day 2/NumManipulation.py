29606291f41dab4ec38139be61f4aad1b4156c6c
sahilsea <sa9504793957@gamil.com>
2023-12-24 13:25:05 +0530

    Add: NumManipulation.py File

---
+++ Day 2/NumManipulation.py
@@ -0,0 +1,12 @@
+print(int(8/3))
+print(round(8/3))
+print(round(8/3, 2))
+print(8//3)
+result = 4/2
+result /= 2
+print(result)
+
+# F Strings - Mix different Data Types
+print(f"your result is {result}, your number is none")
+
+
