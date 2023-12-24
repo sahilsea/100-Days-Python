112363dc4ea4c76574918ca03b9da95cb965c8c4
sahilsea <sa9504793957@gamil.com>
2023-12-24 09:24:02 +0530

    Added: Project TipCalc.py

---
+++ Day 2/TipCalc.py
@@ -0,0 +1,11 @@
+# Welcome 
+print("Welcome to the tip calculator.")
+# Total Bill
+bill = int(input("What is your total bill? " + "$"))
+# Number of people to Split the Bill 
+numOfPpl = int(input("How many people to Split the bill? "))
+# Percentage to Tip
+tip = int(input("What percentage would you like to tip? "))
+# Each Person should pay
+pay = (((bill+((tip)/100))/numOfPpl))
+print ("Each person should pay: " + "$" ,  pay)
