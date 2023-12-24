c3f4f28b58066621fea1820411b41c7563d20e31
sahilsea <sa9504793957@gamil.com>
2023-12-24 12:33:51 +0530

    Add: TypeConversion File

---
+++ Day 2/TypeConversion.py
@@ -0,0 +1,10 @@
+num_char = len(input("What is your name? "))
+#print(type(num_char))
+
+str_num_char = str(num_char)
+print("Your name has " + str_num_char + " characters")
+
+a = 123
+#print(type(a))
+
+print(float(a) + float(123.323))
