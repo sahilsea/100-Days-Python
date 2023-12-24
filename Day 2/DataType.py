35b1401994118b9742e66e7b9100cacbef551af6
sahilsea <sa9504793957@gamil.com>
2023-12-24 12:16:52 +0530

    Add: DataType File

---
+++ Day 2/DataType.py
@@ -0,0 +1,13 @@
+# Python primitive Data type 
+# String
+print("Hello"[3])
+# Intiger
+print(1+2)
+print(12_53)
+# Float
+# Being able to float around numbers
+print(1.3)
+print(23_34.43)
+# Boolean
+print(True)
+print(False)
