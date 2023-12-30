541cdd837b81a2067527b09fec91b31b35f081e2
sahilsea <sa9504793957@gamil.com>
2023-12-30 09:28:27 +0530

    Add: Caesar Cipher logo

--- Day7/Udmy_CeaserCipher.py
+++ Day7/Udmy_CeaserCipher.py
@@ -1,5 +1,7 @@
+import art
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 # Variables to get user input
+print(art.logo)
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
