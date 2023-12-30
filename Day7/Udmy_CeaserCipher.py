fd799d6067d86374bdfcb644276f505a04f5981b
sahilsea <sa9504793957@gamil.com>
2023-12-30 10:19:50 +0530

    Fix : take symbols too now

--- Day7/Udmy_CeaserCipher.py
+++ Day7/Udmy_CeaserCipher.py
@@ -5,22 +5,24 @@ print(art.logo)
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
-shift = shift % 26
+shift = shift % 25
 # Function for encripting the text
 def ceaser(text, shift, direction):
     # Getting the index of all the text given by the user and storing it in list.
     # Shifting the index by user request by adding the shift variable
     cypher = ""
+    if direction == "decode":
+        shift *= -1
     for letter in text:
-        if direction == "encode":
+        if letter in alphabet:
             text_index = alphabet.index(letter) + shift
-        elif direction == "decode":
-            text_index = alphabet.index(letter) - shift
-        if text_index > 25:
-            new_letter = alphabet[text_index-26]
+            if text_index > 25:
+                new_letter = alphabet[text_index-26]
+            else:
+                new_letter = alphabet[text_index]
+            cypher += new_letter
         else:
-            new_letter = alphabet[text_index]
-        cypher += new_letter
+            cypher += letter
     print(f"Your {direction}d text is : {cypher}")
 
 ceaser(text, shift, direction)
