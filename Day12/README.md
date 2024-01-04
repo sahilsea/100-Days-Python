61d76f8c00a4feb7dfb763592b3423084974c2eb
sahilsea <sa9504793957@gamil.com>
2024-01-04 23:10:00 +0530

    Add - Day12

---
+++ Day12/README.md
@@ -0,0 +1,9 @@
+1. Debugged a for loop.
+2. Bugs occuring randomly.
+3. Fixed Errors on consol.
+4. Print is our Best Friend for Debugging.
+5. Use A debugger.
+6. Debugging Exercises-
+    - Odd or Even
+    - Leap year or not
+    - Fizbuzz
---
+++ Day12/small_game_data.py
@@ -0,0 +1,17 @@
+data = [
+    {
+        'name': 'Instagram',
+        'follower_count': 346,
+        'description': 'Social media platform',
+        'country': 'United States'
+    },
+    {
+        'name': 'Cristiano Ronaldo',
+        'follower_count': 215,
+        'description': 'Footballer',
+        'country': 'Portugal'
+    }]
+
+
+for key in data:
+    print(data[key])
