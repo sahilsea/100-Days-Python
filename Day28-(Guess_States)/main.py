53abdfd072a1808fb7458c5ef09dd3ad0f9b5d3b
sahilsea <sa9504793957@gamil.com>
2024-01-21 22:25:40 +0530

    Add: Day28

---
+++ Day28-(Guess_States)/main.py
@@ -0,0 +1,57 @@
+import turtle
+import pandas
+
+# Create the Screen for the Game
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = "bihar90.gif"
+screen.setup(width= 1000)
+screen.addshape(image)
+turtle.shape(image)
+
+
+# Challange 
+score = 0
+guessed_state = []
+data = pandas.read_csv("Bihar_states_coordinates.csv")
+# Getting answer By providing a prompt.
+# Storing all states in a list
+all_states = data.state.to_list()
+
+
+# Checking if the state that user entered , exist in the list of all states
+while len(guessed_state) < 50:
+    answer = screen.textinput(title= f"Guessed State = {score}/50", prompt= "Guess another State" ).title()
+    if answer == "Exit":
+        break
+    if answer in all_states:
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        # Getting the data of the state entered by the user
+        state_i = data[data['state'] == answer]
+        t.goto(x= int(state_i.x), y= int(state_i.y))
+        t.write(arg= f'{answer}', move= False, align= 'center', font= ("Bold", 19, "normal"))
+        guessed_state.append(answer)
+        score += 1
+
+# States that you missed
+missed_state = []
+for state in all_states:
+    if state not in guessed_state:
+        missed_state.append(state)
+
+missed_state_Frame = pandas.DataFrame(missed_state)
+missed_state_Frame.to_csv("States_to_learn.csv")
+
+
+
+# def get_click_coor(x,y):
+#     print(x,y)
+
+# turtle.onscreenclick(get_click_coor)
+# turtle.mainloop()
+
+
+
+screen.exitonclick()
