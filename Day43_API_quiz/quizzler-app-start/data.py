d0df3e12d63f43f5f0c9c3c7d7460dc803393b33
sahilsea <sa9504793957@gamil.com>
2024-02-10 00:33:49 +0530

    Add Day43

---
+++ Day43_API_quiz/quizzler-app-start/data.py
@@ -0,0 +1,10 @@
+import requests
+
+
+response = requests.get(url="https://opentdb.com/api.php?amount=5&difficulty=easy&type=boolean")
+response.raise_for_status()
+data = response.json()
+
+# print(data['results'])
+
+question_data = data['results']
